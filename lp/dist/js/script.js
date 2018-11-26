74fbcf93ccceb1ca814463de2ffe28b0ffae5b31
q3spxx <q3spxx@bk.ru>
2018-11-27 00:51:00 +0400

    add get

--- lp/dist/js/script.js
+++ lp/dist/js/script.js
@@ -157,11 +157,10 @@ const init = () => {
     window.auth = () => {
         const headers = new Headers();
         headers.append('Accept', '*/*');
-        fetch('https://pitstopcafe.amocrm.ru/private/api/auth.php', {
+        fetch('https://pitstopcafe.amocrm.ru/private/api/auth.php?type=json&USER_LOGIN=np@pitstop-cafe.ru&USER_HASH=b49967eaa6c9a2d04d347b0e544bb8877a604c48', {
             headers: headers,
-            method: 'POST',
+            method: 'GET',
             mode: 'no-cors',
-            body: 'USER_LOGIN=np@pitstop-cafe.ru&USER_HASH=b49967eaa6c9a2d04d347b0e544bb8877a604c48',
         }).then(res => console.log(res))
     }
 };
