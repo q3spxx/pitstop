cddefc7067d44ed519ec7031bb3eb2b4e9743c1a
q3spxx <q3spxx@bk.ru>
2018-11-26 23:21:08 +0400

    request test

--- lp/dist/js/script.js
+++ lp/dist/js/script.js
@@ -87,6 +87,78 @@ const init = () => {
     initAccordion(titles, content);
     initTabulation(titles, content);
     initMenuButton();
+
+    window.sendForm = () => {
+        fetch('https://pitstopcafe.amocrm.ru/api/v2/leads', {
+            method: 'POST',
+            body: JSON.stringify({
+                add: [
+                   {
+                      name: "Покупка карандашей",
+                      created_at: "1508101200",
+                      updated_at: "1508274000",
+                      status_id: "13670637",
+                      responsible_user_id: "957083",
+                      sale: "5000",
+                      tags: "pencil, buy",
+                      contacts_id: [
+                         "1099149"
+                         ],
+                         company_id: "1099148",
+                         custom_fields: [
+                            {
+                               id: "4399649",
+                               values: [
+                                  "3691615",
+                                  "3691616",
+                                  "3691617"
+                               ]
+                            },
+                            {
+                               id: "4399656",
+                               values: [
+                                  {
+                                     value: "2017-10-26"
+                                  }
+                               ]
+                            },
+                            {
+                               id: "4399655",
+                               values: [
+                                  {
+                                     value: "ул. Охотный ряд, 1",
+                                     subtype: "address_line_1"
+                                  },
+                                  {
+                                     value: "Москва",
+                                     subtype: "city"
+                                  },
+                                  {
+                                     value: "101010",
+                                     subtype: "zip"
+                                  },
+                                  {
+                                     value: "RU",
+                                     subtype: "country"
+                                  }
+                               ]
+                            }
+                         ]
+                   }
+                ]
+             }),
+        }).then((res) => console.log(res));
+    };
+
+    window.auth = () => {
+        fetch('https://pitstopcafe.amocrm.ru/private/api/auth.php', {
+            method: 'POST',
+            body: JSON.stringify({
+                USER_LOGIN: 'np@pitstop-cafe.ru',
+                USER_HASH: 'b49967eaa6c9a2d04d347b0e544bb8877a604c48',
+            }),
+        }).then(res => console.log(res))
+    }
 };
 
 window.onload = () => {
